5207d12916f678c1beb474641aba1d4281f89f50
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2022-01-19 14:33:54 +0900

    dfjoaef

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import json
 import requests
 import datetime
 
+
 def returnData(data):
     data = data.replace('"', "")
     data = data.replace('\n', "")
@@ -18,54 +19,74 @@ def returnData(data):
     data = data.split(',')
     return data
 
-def main():
-    name= requests.get("http://3359jun.pythonanywhere.com/findUser").text
-    tampTotal = list()
-    name = returnData(name)
-    print(name)
-    for i in range(0, len(name)):
-        print(name[i])
-        changeName = urllib.parse.quote(name[i])
-        html = urlopen("https://lostark.game.onstove.com/Profile/Character/" + changeName)
-        bsObject = BeautifulSoup(html, "html.parser")
-        nickName = bsObject.select('#expand-character-list > ul >li>span>button>span')
-        for j in nickName:
-            changeName = urllib.parse.quote(j.text)
-            html = urlopen("https://lostark.game.onstove.com/Profile/Character/" + changeName)
-            bsObject = BeautifulSoup(html, "html.parser")
-            nickName = j.text
-            itamLevel =str( bsObject.select(
-                '#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__item > span:nth-child(2)'))
+def returnText(data):
+    data = re.sub('<.+?>', '', data, 0).strip()
+    data = data.replace('[', '')
+    data = data.replace(']', '')
+    data = data.replace('Lv.', '')
+    data = data.replace(',', '')
+    return  data
 
-            itamLevel = re.sub('<.+?>', '', itamLevel, 0).strip()
-            itamLevel = itamLevel.replace('[','')
-            itamLevel = itamLevel.replace(']','')
-            itamLevel = itamLevel.replace('Lv.','')
-            itamLevel = itamLevel.replace(',','')
-            itamLevel = itamLevel[0:itamLevel.find('.')]
-            level =str( bsObject.select(
-                '#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info > div.level-info__item > span:nth-child(2)'))
-            level = re.sub('<.+?>', '', level, 0).strip()
-            level = level.replace('[','')
-            level = level.replace(']','')
-            level = level.replace('Lv.','')
-            job =str([imgData.find('img')['alt'] for imgData in
-                   bsObject.select('#lostark-wrapper > div > main > div > div.profile-character-info')])
-            job = job.replace('[','')
-            job = job.replace(']','')
-            job = job.replace("'","")
-            if int(level) >=50:
-                data = [nickName, int(itamLevel), job, i]
+def getName():
+    name= requests.get("http://3359jun.pythonanywhere.com/findUser").text
+    name = returnData(name)
+    return  name
+
+def getCharacter(name):
+    changeName = urllib.parse.quote(name)
+    html = urlopen("https://lostark.game.onstove.com/Profile/Character/" + changeName)
+    bsObject = BeautifulSoup(html, "html.parser")
+    nickName = bsObject.select('#expand-character-list > ul >li>span>button>span')
+    return nickName
+
+def searchCharacter(nickName,i):
+    changeNickName =  urllib.parse.quote(nickName)
+    html = urlopen("https://lostark.game.onstove.com/Profile/Character/" + changeNickName)
+    bsObject = BeautifulSoup(html, "html.parser")
+    itameLevel = str(bsObject.select('#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__item > span:nth-child(2)'))
+    itameLevel = returnText(itameLevel)
+    itameLevel = itameLevel[0:itameLevel.find('.')]
+    level = str( bsObject.select('#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info > div.level-info__item > span:nth-child(2)'))
+    level = returnText(level)
+    job =str([imgData.find('img')['alt'] for imgData in bsObject.select('#lostark-wrapper > div > main > div > div.profile-character-info')])
+    job = job.replace('[', '')
+    job = job.replace(']', '')
+    job = job.replace("'", "")
+    print(level)
+    if int(level) >= 50:
+        data = [nickName, int(itameLevel), job, i]
+        return data
+
+def getData(a,b,name):
+    tampTotal = list()
+    for i in range(a,b):
+        character = getCharacter(name[i])
+        for j in character:
+            data = searchCharacter(j.text, i)
+            if data != None:
                 tampTotal.append(data)
     return tampTotal
 
-total = main()
+
+
+now = datetime.datetime.now()
+current_time = now.strftime("%M")
+name = getName()
+num = int(len(name)/3)
+
+if int(current_time)<=20:
+    total = getData(0,num,name)
+elif int(current_time) <=40:
+    total = getData(num,num*2,name)
+else:
+    total = getData(num*2, len(name), name)
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 now = datetime.datetime.now()
 tempArray = []
 tempArray.append(str(now))
 total.append(tempArray)
+print(total)
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
     json.dump(total, json_file, ensure_ascii = False, indent='\t')
-
 res = requests.post("http://3359jun.pythonanywhere.com/sendUserData", data=json.dumps(total))
